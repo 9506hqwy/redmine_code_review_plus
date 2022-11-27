84fc123b0e147c2141f1db50ae63c983a2b4b9f7
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-11-27 11:57:06 +0900

    Redmine5 でモジュールのロードが失敗する問題を修正した。

--- lib/redmine_code_review_plus/attachments_helper_patch.rb
+++ lib/redmine_code_review_plus/attachments_helper_patch.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+module RedmineCodeReviewPlus
+  module AttachmentsHelperPatch
+    # Dummy module for Zeitwerk mode.
+  end
+end
+
 Rails.application.config.after_initialize do
   AttachmentsController.send(:helper, :issues)
   AttachmentsController.send(:helper, :queries)
